f91a780be5bf7c2a996f7f10d07813f00fbc2281
aliraad79 <aliraad79@gmail.com>
2020-11-27 19:26:01 +0330

    change test and change output

--- OOP/main.py
+++ OOP/main.py
@@ -3,12 +3,12 @@ from Advertiser import Advertiser
 from BaseAdvertising import BaseAdvertising
 
 
-baseAdvertising = BaseAdvertising()
+# baseAdvertising = BaseAdvertising()   abstract class can't be instanced
 advertiser1 = Advertiser(1, "name1")
 advertiser2 = Advertiser(2, "name2")
 ad1 = Ad(1, "title1", "img-url1", "link1", advertiser1)
 ad2 = Ad(2, "title2", "img-url2", "link2", advertiser2)
-print(baseAdvertising.describeMe())
+# print(baseAdvertising.describeMe())
 print(ad2.describeMe())
 print(advertiser1.describeMe())
 ad1.incViews()
@@ -25,4 +25,4 @@ print(advertiser2.getName())
 print(ad1.getClicks())
 print(advertiser2.getClicks())
 print(Advertiser.getTotalClicks())
-print(Advertiser.help())
+# print(Advertiser.help())              help is not static
